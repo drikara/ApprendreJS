2462d1363519950148b599e45aca741e4afb2ca0
drikara <ouattara17drissa@gmail.com>
2025-08-09 00:21:54 +0000

    j'ai appris les variables en js

---
+++ scripts/variable.js
@@ -0,0 +1,14 @@
+let nom = "jean philippe";
+let a = 5;
+let b = 5;
+let res = a + b ;
+let bool = true;
+let tab = ["jean", "philippe", 5, 8];
+/* voici comment on fait un commentaire multilignes en js */
+// voici comment faire un commentaire une seule ligne
+alert(nom);
+alert(res);
+alert(bool);
+alert(tab[0]);
+
+
