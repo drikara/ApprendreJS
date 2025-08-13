6f44d46ea17c12803e607ca1fcc61d52551758aa
drikara <ouattara17drissa@gmail.com>
2025-08-13 23:43:36 +0000

    j'ai appris la programmation fonctionnelle

--- scripts/progFonct.js
+++ scripts/progFonct.js
@@ -4,3 +4,29 @@ function add(a,b){
     
 }
 console.log(add(4,5));
+// immuabilité
+const nombres = [1,2,3];
+console.log(nombres);
+
+const nouveauNombres = [...nombres,4];
+console.log(nouveauNombres);
+
+// fonction d'ordre supérieur
+function multiplierPar(facteur){
+    return function(nombre){
+        return nombre * facteur;
+    }
+}
+
+const double = multiplierPar(2);
+console.log(double(3));
+
+// composition
+const composer = (...fns) => x => fns.reduceRight((v,f) => f(v),x);
+const ajouter5 = x => x+5;
+const multiplierPar2 = x => x * 2;
+const transformer = composer(ajouter5,multiplierPar2);
+console.log(transformer(4)); //(4*2)+5 =13
+
+
+
