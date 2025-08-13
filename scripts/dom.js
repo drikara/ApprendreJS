35044ea5c454608ff2648b9ef9be5cd42084b706
drikara <ouattara17drissa@gmail.com>
2025-08-13 23:29:08 +0000

    je suis entraine d'apprendre la programmation fonctionnelle

--- scripts/dom.js
+++ scripts/dom.js
@@ -1,8 +1,18 @@
 // selection
 
 let h1 = document.querySelector("h1");
-let maclasse =document.getElementsByClassName("maClasse")
+let maclasse =document.getElementsByClassName("maClasse");
  //modification
  h1.innerHTML = "Présentation <em>data</em>";
  maclasse.innerText = "<strong>papa vient à la maison</strong>"
  h1.style.color = "red";
+ h1.classList.add("titre1");
+ // évenement
+ h1.addEventListener("click",(e)=>{
+    console.log("click detecté", e.target);
+    
+ })
+// création
+const nouveauDiv = document.createElement("div");
+nouveauDiv.textContent = "nouvelle div";
+document.body.appendChild(nouveauDiv);
